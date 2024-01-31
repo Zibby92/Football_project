c36c8850fd240db28ad2dba6420c0b003c1ea36a
Zibby92 <119049399+Zibby92@users.noreply.github.com>
2024-01-31 20:06:53 +0100

    add procedure show_teams_against

--- PACKAGES/pkg_player_statistics.sql
+++ PACKAGES/pkg_player_statistics.sql
@@ -1,12 +1,13 @@
 CREATE OR REPLACE PACKAGE pkg_player_statistics IS
 
-    PROCEDURE show_amount_of_goals_and_average_time (in_player_name VARCHAR2); 
+    PROCEDURE p_show_amount_of_goals_and_average_time (in_player_name VARCHAR2); 
+    PROCEDURE p_show_teams_against(in_player_name VARCHAR2);
 
 END pkg_player_statistics;
 /
 CREATE OR REPLACE PACKAGE BODY pkg_player_statistics IS
 
-    PROCEDURE show_amount_of_goals_and_average_time (in_player_name VARCHAR2) 
+    PROCEDURE p_show_amount_of_goals_and_average_time (in_player_name VARCHAR2) 
     IS
         v_player_name VARCHAR2(50) := pkg_check_data.f_check_scorer(in_player_name);
         TYPE r_amount_and_time IS RECORD (amount_of_goals NUMBER, average_time NUMBER);
@@ -21,7 +22,30 @@ CREATE OR REPLACE PACKAGE BODY pkg_player_statistics IS
         CLOSE c_get_data;
         DBMS_OUTPUT.PUT_LINE('Player '|| v_player_name || ' shooted: ' || v_r_amount_and_time.amount_of_goals 
                             || ' . His average minute of goal is: '|| v_r_amount_and_time.average_time);
-    END show_amount_of_goals_and_average_time;
-
-
+    END p_show_amount_of_goals_and_average_time;
+    
+    PROCEDURE p_show_teams_against(in_player_name VARCHAR2) 
+    IS
+        v_player_name VARCHAR2(50) := pkg_check_data.f_check_scorer(in_player_name);
+        TYPE r_teams_and_goals IS RECORD (teams VARCHAR2(70), goals NUMBER);
+        TYPE ntt_r_teams_and_goals IS TABLE OF r_teams_and_goals;
+        v_teams_and_goals ntt_r_teams_and_goals;
+        CURSOR c_teams_and_goals IS
+            SELECT team, COUNT(*) amount FROM 
+                (SELECT CASE WHEN home_team = (SELECT DISTINCT(team) FROM goal_scorers WHERE scorer = v_player_name) THEN away_team 
+                            ELSE home_team END team
+                FROM goal_scorers
+                WHERE scorer = v_player_name) Q
+                GROUP BY team
+                ORDER BY amount DESC;
+        BEGIN 
+            OPEN c_teams_and_goals;
+            FETCH c_teams_and_goals BULK COLLECT INTO v_teams_and_goals;
+            CLOSE c_teams_and_goals;
+            DBMS_OUTPUT.PUT_LINE('Teams and amount of goals against which player ' || v_player_name || ' shooted');
+            FOR i IN 1..v_teams_and_goals.LAST
+                LOOP
+                    DBMS_OUTPUT.PUT_LINE(v_teams_and_goals(i).teams ||' - ' || v_teams_and_goals(i).goals);
+                END LOOP;
+        END p_show_teams_against;
 END pkg_player_statistics;
